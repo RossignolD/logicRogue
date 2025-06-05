9b33e792423fa249f029b9035ea83c79ff1f80ac
RossignolD <knightsoflogicland@gmail.com>
2025-06-05 15:50:01 -0400

    added coverage to jest tests

--- logic/naturalDeduction2.test.js
+++ logic/naturalDeduction2.test.js
@@ -10,7 +10,35 @@ test("natural deduction contains correct show line", () => {
   expect(nd).toHaveProperty("showLine", "Q");
 });
 
-test("natural deduction contains correct show line", () => {
+test("natural deduction contains correct premises", () => {
   const nd = new NaturalDeduction("Q", ["P-Q", "P"]);
   expect(nd).toHaveProperty("premises", ["P-Q", "P"]);
+});
+
+test("premises can be loaded in as lines in a natural deduction", () => {
+  const nd = new NaturalDeduction("Q", ["P-Q", "P"]);
+  nd.addPremises();
+  expect(nd.lines).toContainEqual({
+    tree: { "ONLY IF": [{ atomic: "P" }, { atomic: "Q" }] },
+    justification: "Premise",
+  });
+});
+
+test("can add an assumption for CD at start of deduction", () => {
+  const nd = new NaturalDeduction("P-R", ["P-Q", "P-R"]);
+  nd.addAssumption("CD");
+  expect(nd.lines).toContainEqual({
+    tree: { atomic: "P" },
+    justification: "Asm. CD",
+  });
+});
+
+test("can complete a natural deduction by DD", () => {
+  const nd = new NaturalDeduction("Q", ["(~(P-Q))-(~R)", "R", "P"]);
+  nd.addPremises();
+  nd.addLine("DNI", "R");
+  nd.addLine("MT", "(~(P-Q))-(~R)", "~~R");
+  nd.addLine("DNE", "~~(P-Q)");
+  nd.addLine("MP", "P-Q", "P");
+  expect(nd.addLine("MP", "P-Q", "P")).toBe("Proof completed");
 });
