ec4ffbbe6478114dd3560eb46335cd4f695ed2b9
RossignolD <knightsoflogicland@gmail.com>
2025-06-06 16:24:49 -0400

    work on encounter component ongoing
    Co-authored-by: Josie-Christian jchrist3099@gmail.com
    Co-authored-by: Scott Webster webster.j.scott@gmail.com

--- frame/src/Encounter.jsx
+++ frame/src/Encounter.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Encounter.module.css";
 import { NaturalDeduction } from "../../logic/naturalDeduction2";
 import encounters from "../../encounters.json";
 
+const [inventoryItems, setInventoryItems] = useState([]);
+const [newItemName, setNewItemName] = useState("");
+const [rulesText, setRulesText] = useState([]);
+
 console.log(encounters);
 
 function Encounter({ encounterName }) {
+  useEffect(() => {
+    setTimeout(() => {
+      iframe.contentWindow.postMessage(
+        "Hello from react",
+        "http://localhost:3001/"
+      );
+    }, 200);
+  });
+
+  const parseTree = { "ONLY IF": [{ atomic: "P" }, { atomic: "Q" }] };
+
+  const handleAddTree = () => {
+    setInventoryItems((prevItems) => [...prevItems, parseTree]);
+  };
+  const handleCodexClick = () => {
+    console.log("you have opened the codex");
+  };
+
   return (
     <div className={styles.Encounter}>
+      <div>This is codex icon</div>
       <div className={styles.showLine}>
         <h1>{encounters[0].showLine}</h1>
       </div>
-      <div className={styles.inventory}>Inventory component here</div>
+      <div className={styles.inventory}>
+        <h2>Encounter Inventory</h2>
+      </div>
       <div className={styles.rules}>Rules unlocked so far here</div>
     </div>
   );
